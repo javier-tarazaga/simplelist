ac107c6c604d2491f9456d4e36ca037b81fa9bbd
Javier Tarazaga <javier-tarazaga@users.noreply.github.com>
2022-10-13 09:27:28 +0200

    Added some more fixes

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ListModule } from './list/list.module';
+import { SimplelistLoggerModule } from '@simplelist/core';
+import { PrismaService } from './db/prisma.service';
 
 @Module({
   imports: [
@@ -13,9 +15,17 @@ import { ListModule } from './list/list.module';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    SimplelistLoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.LOG_LEVEL ?? 'debug',
+      },
+    }),
     AuthModule,
     ListModule,
     UserModule,
   ],
+  providers: [
+    PrismaService
+  ]
 })
 export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,11 +7,17 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { PrismaService } from './app/db/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const globalPrefix = 'graphql';
   app.setGlobalPrefix(globalPrefix);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app)
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
